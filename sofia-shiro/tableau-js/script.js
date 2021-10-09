d7011010e5ddd740223d90431befb51e44979451
Shiropetite <59747311+Shiropetite@users.noreply.github.com>
2021-10-09 21:44:29 +0200

    update tableau js

--- sofia-shiro/tableau-js/script.js
+++ sofia-shiro/tableau-js/script.js
@@ -1,8 +1,28 @@
+// Etape 1.4
 let fruitsList = null
 let input = null
+// Etape 1.3
 let fruits = ['fraise', 'framboise', 'citron' ,'banane', 'poire']
 
+// Etape 1.7
+const updateHTML = () => {
+  // Etape 1.8 - 9
+  let stringFruits = "<ul>"
+
+  fruits.forEach((fruit) => {
+    // Etape 1.9 -> stringFruits += "<li> "+ fruit + "</li>"
+    // Etape 3.4
+    stringFruits += "<li> "+ fruit + "<button class='red' onClick=\"remove('"+ fruit +"')\">X</button> </li>"
+  })
+  stringFruits += "</ul>"
+
+  // Etape 1.10
+  fruitsList.innerHTML = stringFruits
+}
+
+// Etape 4.1
 const filter = () => {
+  // Etape 4.2
   fruits = fruits.sort((a, b) => {
     if (a > b) {
         return 1;
@@ -10,43 +30,37 @@ const filter = () => {
     if (a < b) {
         return -1;
     }
-    console.log(0)
     return 0;
   })
 
-  updateHTML()
+  updateHTML() // Etape 4.3
 }
 
-const add = () => {
-  fruits.push(input.value)
-
-  updateHTML()
-}
-
+// Etape 3.1
 const remove = (element) => {
+  // Etape 3.2
   fruits = fruits.filter((fruit) => fruit !== element)
 
-  updateHTML()
+  updateHTML() // Etape 3.3
 }
 
-const updateHTML = () => {
-  let stringFruits = "<ul>"
-  fruits.forEach((fruit) => {
-    stringFruits += "<li> "+ fruit +" <button class='red' onClick=\"remove('"+ fruit +"')\">X</button> </li>"
-  })
-  stringFruits += "</ul>"
+// Etape 2.1
+const add = () => {
+  fruits.push(input.value) // Etape 2.2
 
-  fruitsList.innerHTML = stringFruits
+  updateHTML() // Etape 2.3
 }
 
-const load = () => {
-  fruitsList = document.getElementById('list')
-  input = document.getElementById('input')
+// Etape 1.1
+const pageLoaded = () => {
+  fruitsList = document.getElementById('list') // Etape 1.5
+  input = document.getElementById('input') // Etape 1.6
 
-  document.getElementById('add').addEventListener('click', add)
+  document.getElementById('add').addEventListener('click', add) // Etape 2.4
   document.getElementById('filter').addEventListener('click', filter)
 
-  updateHTML()
+  updateHTML() // Etape 1.11
 }
 
-window.addEventListener("load", load)
+// Etape 1.2
+window.addEventListener("load", pageLoaded)
